3cd150ce710a225fecbdd68900a776ea13d5761b
Julie Nisbet <jmnisbet@gmail.com>
2022-08-01 11:32:58 -0700

    adds get route and related records on post

--- __tests__/owners.test.js
+++ __tests__/owners.test.js
@@ -8,16 +8,27 @@ describe('backend-express-template routes', () => {
     return setup(pool);
   });
 
-  it('should add a new owner', async () => {
+  it('#GET /:id should return the owner with its pets', async () => {
+    const resp = await request(app).get('/owners/1');
+    console.log(resp.body);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      pets: expect.any(Array),
+    });
+  });
+
+  it('#POST /owners should add a new owner', async () => {
     const resp = await request(app).post('/owners').send({ name: 'Franny' });
     expect(resp.status).toBe(200);
     expect(resp.body).toEqual({
       id: expect.any(String),
       name: expect.any(String),
+      pets: expect.any(Array),
     });
   });
 
-  it('should add a new owner and associate it with pets', async () => {
+  it('#POST /owners should add a new owner and associate it with pets', async () => {
     const resp = await request(app)
       .post('/owners')
       .send({ name: 'Harriet', petIds: [1, 2] });
@@ -25,7 +36,11 @@ describe('backend-express-template routes', () => {
     expect(resp.body).toEqual({
       id: expect.any(String),
       name: expect.any(String),
+      pets: expect.any(Array),
     });
+
+    const ownerResp = await request(app).get(`/owners/${resp.body.id}`);
+    expect(ownerResp.body.pets.length).toBe(2);
   });
 
   afterAll(() => {
